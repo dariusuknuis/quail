be3f7a67a5a75c44727eac7d41495f22f6b7d99f
dariusuknuis <darius_uknuis@yahoo.com>
2026-07-28 17:52:06 -0400

    Update lit read/write

--- raw/lit_read.go
+++ raw/lit_read.go
@@ -15,48 +15,74 @@ type Lit struct {
 	Entries      [][4]uint8
 }
 
-// Identity returns the type of the struct
+// Identity returns the type of the struct.
 func (lit *Lit) Identity() string {
 	return "lit"
 }
 
-// Decode will read a lit
+// Read reads a LIT file.
 func (lit *Lit) Read(r io.ReadSeeker) error {
 	dec := encdec.NewDecoder(r, binary.LittleEndian)
 
+	header := dec.StringFixed(4)
+	if header != "EQGP" {
+		return fmt.Errorf("invalid header %s, wanted EQGP", header)
+	}
+
 	lightCount := dec.Uint32()
-	for i := 0; i < int(lightCount); i++ {
-		lit.Entries = append(lit.Entries, [4]uint8{dec.Uint8(), dec.Uint8(), dec.Uint8(), dec.Uint8()})
+
+	lit.Entries = make([][4]uint8, 0, lightCount)
+
+	for i := uint32(0); i < lightCount; i++ {
+		entry := [4]uint8{
+			dec.Uint8(),
+			dec.Uint8(),
+			dec.Uint8(),
+			dec.Uint8(),
+		}
+
+		lit.Entries = append(lit.Entries, entry)
 	}
 
 	pos := dec.Pos()
+
 	endPos, err := r.Seek(0, io.SeekEnd)
 	if err != nil {
 		return fmt.Errorf("seek end: %w", err)
 	}
+
 	if pos < endPos {
 		remaining := dec.Bytes(int(endPos - pos))
-		if !bytes.Equal(remaining, []byte{0x0, 0x0, 0x0, 0x0}) {
+
+		// Some files may contain one trailing zero DWORD.
+		if !bytes.Equal(remaining, []byte{0x00, 0x00, 0x00, 0x00}) {
 			fmt.Printf("remaining bytes: %s\n", hex.Dump(remaining))
-			return fmt.Errorf("%d bytes remaining (%d total)", endPos-pos, endPos)
+
+			return fmt.Errorf(
+				"%d bytes remaining (%d total)",
+				endPos-pos,
+				endPos,
+			)
 		}
 	}
+
 	if pos > endPos {
 		return fmt.Errorf("read past end of file")
 	}
-	if dec.Error() != nil {
-		return fmt.Errorf("read: %w", dec.Error())
+
+	if err := dec.Error(); err != nil {
+		return fmt.Errorf("read: %w", err)
 	}
 
 	return nil
 }
 
-// SetFileName sets the name of the file
+// SetFileName sets the name of the file.
 func (lit *Lit) SetFileName(name string) {
 	lit.MetaFileName = name
 }
 
-// FileName returns the name of the file
+// FileName returns the name of the file.
 func (lit *Lit) FileName() string {
 	return lit.MetaFileName
 }
--- raw/lit_write.go
+++ raw/lit_write.go
@@ -8,18 +8,26 @@ import (
 	"github.com/xackery/encdec"
 )
 
+// Write writes a complete LIT file.
 func (lit *Lit) Write(w io.Writer) error {
 	enc := encdec.NewEncoder(w, binary.LittleEndian)
+
+	// File identifier
+	enc.StringFixed("EQGP", 4)
+
+	// Number of four-byte lighting entries
 	enc.Uint32(uint32(len(lit.Entries)))
+
 	for _, entry := range lit.Entries {
 		enc.Uint8(entry[0])
 		enc.Uint8(entry[1])
 		enc.Uint8(entry[2])
 		enc.Uint8(entry[3])
 	}
-	err := enc.Error()
-	if err != nil {
+
+	if err := enc.Error(); err != nil {
 		return fmt.Errorf("write: %w", err)
 	}
+
 	return nil
 }
